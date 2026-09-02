77708c59cec669993b8ec66cb5c3544e5e81fc2c
jahid <jahidhasanbsl46@gmail.com>
2026-09-02 15:40:18 +0600

    update cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000'],
+    'allowed_origins' => ['https://dreem-walker-client.vercel.app', 'http://localhost:3000', 'http://localhost:3001'],
 
     'allowed_origins_patterns' => [],
 
